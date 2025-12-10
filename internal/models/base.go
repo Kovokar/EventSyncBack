1fa6be7c8c2f203f706d5126d3741f06d085405c
estagiario.devmed <estagiario.dev.medclub@gmail.com>
2025-12-10 13:26:54 -0300

    feat: created base model

---
+++ internal/models/base.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// BaseModel defines common fields shared across all entities, such as ID,
+// creation/update timestamps, soft delete support, and information about
+// who created, updated, or deleted the record.
+
+type BaseModel struct {
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	CreatedBy *uint          `json:"created_by,omitempty"`
+	UpdatedBy *uint          `json:"updated_by,omitempty"`
+	DeletedBy *uint          `json:"deleted_by,omitempty"`
+}
